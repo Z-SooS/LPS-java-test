98249d619697c931ed6634443f26819804a00b9a
Z-SooS <zoli.soos98@gmail.com>
2023-06-06 16:54:50 +0200

    Set up all objects

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.example.menu.MenuHandler;
+import org.example.ui.ConsoleInput;
+import org.example.ui.ConsoleOutput;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length < 2) {
+            System.out.println("Please enter name and email as arguments!");
+            return;
+        }
+        String name = args[0];
+        String email = args[1];
+
+        HtmlBuilder builder = new HtmlBuilder("");
+        builder.setUpExample(name,email);
+
+        ConsoleInput input = new ConsoleInput(new Scanner(System.in));
+        ConsoleOutput output = new ConsoleOutput();
+
+        MenuHandler menuHandler = new MenuHandler(input, output, builder);
+
+        menuHandler.run();
     }
 }
